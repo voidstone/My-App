14ebf4f17efa4d0b903a9c1828ecaa7402affd8e
Sergey Chaikin <chaikin.sergey@gmail.com>
2019-06-25 14:15:44 +0400

    пару изменений

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
-import Footer from './Footer'
-import Header from './Header';
-import MainContent from './MainContent';
+import Footer from './components/Footer'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeofDay
+
+  if(hours < 12) {
+    timeofDay = "morning"
+  } else if(hours >= 12 && hours < 17) {
+    timeofDay = "afternoon"
+  } else {
+    timeofDay = "night"
+  }
+
+
   return (
-    <div>
-        <Header />
-        <MainContent />
-        <Footer />
-    </div>
+    // <div>
+    //     <Header />
+    //     <MainContent />
+    //     <Footer />
+    // </div>
+    <h1 style={{color:"#FF8C00"}} >It is currently about {timeofDay} o'clock!</h1>
   );
 }
 
