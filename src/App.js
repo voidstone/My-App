96ee4cf8862e56b3d87f20dad6ccac22a8ea91f9
Sergey Chaikin <chaikin.sergey@gmail.com>
2019-06-26 16:24:47 +0400

    очередные изменения

--- src/App.js
+++ src/App.js
@@ -3,28 +3,35 @@ import './App.css';
 import Footer from './components/Footer'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import "./index.css"
 
 function App() {
-  const date = new Date()
+  const styles = {    
+    fontSize: "24px"
+  }
+  const date = new Date(2018, 6, 31,15)
   const hours = date.getHours()
   let timeofDay
 
   if(hours < 12) {
     timeofDay = "morning"
+    styles.color = "#04756F"
   } else if(hours >= 12 && hours < 17) {
     timeofDay = "afternoon"
+    styles.color = "#2E0927"
   } else {
     timeofDay = "night"
+    styles.color = "#D90000"
   }
 
 
   return (
-    // <div>
-    //     <Header />
-    //     <MainContent />
-    //     <Footer />
-    // </div>
-    <h1 style={{color:"#FF8C00"}} >It is currently about {timeofDay} o'clock!</h1>
+    <div>
+        <Header />
+        <MainContent />
+        <Footer />
+    </div>
+    //<h1 style={styles} >It is currently about {timeofDay} o'clock!</h1>
   );
 }
 
